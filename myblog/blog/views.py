82554feaacff1eb4ee84030175ad2c4c563205c7
jingyu323 <xzg323@163.com>
2018-08-27 22:07:31 +0800

    添加template

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from blog.models import BlogsPost
+# Create your views here.
+def blog_index(request):
+    blog_list = BlogsPost.objects.all()  # 获取所有数据
+    return render(request,'index.html', {'blog_list':blog_list})
