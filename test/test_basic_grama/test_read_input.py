33c61f2fa2d15ed9cd071bd6fc68c8c7d74b742b
Rain <xzg323@163.com>
2022-05-05 20:40:45 +0800

    test input

--- test/test_basic_grama/test_read_input.py
+++ test/test_basic_grama/test_read_input.py
@@ -2,7 +2,27 @@
 # -*- coding: UTF-8 -*-
 
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 
 if __name__ == '__main__':
     str = input("请输入：")
     print("你输入的内容是: ", str)
+
+    # 打开数据库连接
+    db = pymysql.connect("localhost", "root", "root", "tets")
+
+    # 使用 cursor() 方法创建一个游标对象 cursor
+    cursor = db.cursor()
+
+    # 使用 execute() 方法执行 SQL 查询
+    cursor.execute("SELECT VERSION()")
+
+    # 使用 fetchone() 方法获取单条数据.
+    data = cursor.fetchone()
+
+    print("Database version : %s " % data)
+
+    # 关闭数据库连接
+    db.close()
