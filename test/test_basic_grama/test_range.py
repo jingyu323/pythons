998afe30e93f0c01b1fc53e4e521fa29a59b2be2
Rain <xzg323@163.com>
2022-05-20 23:18:36 +0800

    test input

---
+++ test/test_basic_grama/test_range.py
@@ -0,0 +1,12 @@
+
+for i in range(10):
+    print(i)
+
+
+for i in range(1, 20, 2):
+    print(i, end=" ")
+
+for i in range(1, 6):
+    for j in range(6-i, 0, -1):
+        print("*", end=" ")
+    print()
