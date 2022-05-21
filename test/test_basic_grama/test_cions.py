831e692003a6a75cfca76528e6a91a4668415d0e
Rain <xzg323@163.com>
2022-05-21 16:21:08 +0800

    修改为python3的语法

--- test/test_basic_grama/test_cions.py
+++ test/test_basic_grama/test_cions.py
@@ -23,7 +23,7 @@
 
 def change_cion():
     from pip._vendor.distlib.compat import raw_input
-    n = int( raw_input('input a number:'))
+    n = int( input('input a number:'))
     sum = 0;
     threee_max = n//3 +1
     for i in range(threee_max):
