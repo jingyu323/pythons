aaf473e06f200bb6208b25ac38dfc67465de7c19
Rain <xzg323@163.com>
2022-05-27 23:19:04 +0800

    修改为python3的语法

--- test/test_basic_grama/base_data_type.py
+++ test/test_basic_grama/base_data_type.py
@@ -1,7 +1,5 @@
 
 # 数组 list
-
-
 list_data  = [1, 2, 3, 4]
 
 list_data.append(9)
@@ -14,6 +12,12 @@ list_data.insert(4,8)
 print(list_data)
 
 list_data[3]=44
-
-
 print(list_data)
+
+# 多个变量定义 ,总结定义多个变量只需要一个等号 按照顺序赋值就行
+
+a, b, c = 1, 2, "john"
+print(a)
+print(c)
+
+
