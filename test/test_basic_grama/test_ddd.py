078359a4ab322372c9a38e3e6f67ecb92e3022ae
Rain <xzg323@163.com>
2022-05-29 08:35:06 +0800

    基本类型 复习 77 练习题

--- test/test_basic_grama/test_ddd.py
+++ test/test_basic_grama/test_ddd.py
@@ -2,3 +2,36 @@ sss="sjjsjsjjs"
 sarr=list(sss)
 for ss in sarr:
     print(ss)
+
+count = 30888 #麦叔粉丝数
+
+# 关注
+def guanzhu():
+    global count
+    count = count + 1
+    print('麦叔的粉丝数是{}'.format(count))
+
+# 取关
+def quguan():
+    global count
+    count = count - 1
+    print('麦叔的粉丝数是{}'.format(count))
+
+guanzhu()
+
+quguan()
+
+
+countsss = "ssssss" #麦叔粉丝数
+
+# 关注
+def guanzhu3333():
+    global countsss
+    countsss = countsss + "------"
+    print('----{}'.format(countsss))
+
+
+guanzhu3333()
+
+print("countsss："+countsss)
+
