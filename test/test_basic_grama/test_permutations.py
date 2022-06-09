8f56b6ea0cd4f397fba1f7f474c40b45107f952c
Rain <xzg323@163.com>
2022-06-09 23:13:05 +0800

    方法内部传参数修改内部变量

--- test/test_basic_grama/test_permutations.py
+++ test/test_basic_grama/test_permutations.py
@@ -1,2 +1,7 @@
-import  itertools
+from itertools import permutations
 from functools  import  cmp_to_key
+arr = [1, 2, 3, 4]
+for i in permutations([1, 2, 3, 4], 3):
+    print(''.join(map(str, i)))
+
+arr.sort(key=cmp_to_key())
