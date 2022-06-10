1d9b3c9594a0fce18a54534bafda6bcf9ef50bf1
Rain <xzg323@163.com>
2022-06-10 22:35:45 +0800

    方法内部传参数修改内部变量

--- test/test_basic_grama/study_exam/bei_bao.py
+++ test/test_basic_grama/study_exam/bei_bao.py
@@ -12,3 +12,32 @@ for i in range(len(u)):
     q = [u[i], v[i]]
     a.append(q)
 a=list(permutations(a))
+
+al=[]
+for i in a:
+    al.append(list(i))
+
+print(al)
+
+gdl = []
+for x in al:  # x是一种情况，示例：[[1(重量),2(价值)],[1(重量),2(价值)]]
+    jz = 0
+    wt = 0
+    jq = len(x) - 1
+    for y in x:  # y是一组数据，示例：[1(重量),2(价值)]
+
+        jz += y[1]
+        wt += y[0]
+
+    while True:
+
+        if wt > s:
+            wt -= x[jq][0]
+            jz -= x[jq][1]
+            jq -= 1
+
+        if wt <= s:
+            gdl.append(jz)
+            break
+
+print(max(gdl))
