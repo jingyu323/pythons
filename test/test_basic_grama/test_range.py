ff1f1b268da3b265344d7bbaa7ce87d0adee3ac4
Rain <xzg323@163.com>
2022-06-12 19:24:42 +0800

    方法内部传参数修改内部变量

--- test/test_basic_grama/test_range.py
+++ test/test_basic_grama/test_range.py
@@ -10,3 +10,12 @@ for i in range(1, 6):
     for j in range(6-i, 0, -1):
         print("*", end=" ")
     print ()
+
+d = {'a':'apple', 'b':'banana', 'c':'car', 'd': 'desk'}
+for key in d:
+    # 遍历字典时遍历的是键
+    print(key, d.get(key))
+# for key, value in d.items():
+# 上下两种方式等价 d.items() <=> dict.items(d)
+for key, value in dict.items(d):
+    print(key, value)
