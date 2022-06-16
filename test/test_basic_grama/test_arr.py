a43a066d47bcc8ec0e6bec940073c99c7f04d70e
Rain <xzg323@163.com>
2022-06-16 23:47:20 +0800

    方法内部传参数修改内部变量

--- test/test_basic_grama/test_arr.py
+++ test/test_basic_grama/test_arr.py
@@ -14,4 +14,10 @@ dp = [1] * len(list)
 
 print(dp)
 
-index = bisect.bisect_left(res, arr[i])
+# index = bisect.bisect_left(res, arr[i])
+
+import numpy as np
+mylist = [1,2,3]
+print(tuple(mylist))
+iarray = np.array(tuple(mylist))
+print( iarray)
