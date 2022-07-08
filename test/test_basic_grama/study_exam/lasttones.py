80fbd5c4a3dff4f9abc9c601f3c18d4165748751
Rain <xzg323@163.com>
2022-07-08 23:34:30 +0800

    方法内部传参数修改内部变量

--- test/test_basic_grama/study_exam/lasttones.py
+++ test/test_basic_grama/study_exam/lasttones.py
@@ -1,2 +1,35 @@
-# 最后一块石头的重量 II
+# 最后一块石头的重量 II LeetCode刷题之路：1049. 最后一块石头的重量 II
+"""
+targetSum =  sum // 2 ; 整除
+ for i in range(targetSum,0,-1):  反向 递减
 
+array = [  0 for j in range (targetSum+1)]  初始化数据
+  dp = [[0 for j in range(V+1)] for i in range(n+1)]
+
+"""
+
+def lastStoneWeightII(stones):
+    sum = 0;
+    st_len = len(stones)
+    for i in range(st_len) :
+        sum += stones[i];
+
+    targetSum =  sum // 2 ;
+    array = [  0 for j in range (targetSum+1)]
+
+    for  j in range(st_len):
+        for i in range(targetSum,0,-1):
+            without_re = array[i]
+            if stones[j] <= i:
+                with_re =stones[j] + array[i - stones[j]]
+            else:
+                with_re = array[i]
+            array[i] = max(without_re, with_re);
+
+
+    return sum - 2 * array[targetSum];
+
+
+stones = [31,26,33,21,40]
+res =lastStoneWeightII(stones)
+print(res)
