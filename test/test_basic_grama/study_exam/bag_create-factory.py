94252e261b34c636bf859658198c777a73556007
Rain <xzg323@163.com>
2022-07-10 16:43:39 +0800

    ``

--- test/test_basic_grama/study_exam/bag_create-factory.py
+++ test/test_basic_grama/study_exam/bag_create-factory.py
@@ -5,7 +5,7 @@
 """
 
 
-item = []
+
 w = [0, 15, 10, 12, 8];
 v = [0, 12, 8, 9, 5]
 
@@ -15,18 +15,25 @@ c = 30;
 n = 4;
 v_len = len(v)
 
-N=150
-dp = [[0 for j in range(N)] for i in range(N)]
-
+dp = [[0 for j in range(c+1)] for i in range(n+1)]
+item = [0 for j in range (5)]
 def findWaht(i, j):
-    if i >= 0:
-        if dp[i][j] == dp[i - 1][j]:
-            item[i] = 0;
-            findWaht(i - 1, j);
-    else:
-        if j - w[i] >= 0 and dp[i][j] == dp[i - 1][j - w[i]] + v[i]:
-            item[i] = 1;
-            findWaht(i - 1, j - w[i]);
+    for i in range(i,0,-1):
+        if dp[i][j] == dp[i-1][j]:
+            item[i] = 0
+        else:
+            item[i] = 1
+            j = j-w[i]
+    # if dp[1][j] > 0:
+    #     item[1] = 1
+    # else:
+    #     item[1] = 0
+
+    print(item)
+
+
+
+
 
 
 
@@ -38,11 +45,7 @@ def findMin():
             if col >= w[row]:
                 res  = max(dp[row-1][col],dp[row -1][col -w[row] ]+ v[row])
                 dp[row][col] = res
-
-
-
                 print("res =",res)
-                print("dp =",dp)
                 print(" dp[row][col] =", dp[row][col])
             else:
                 print("sss else:", dp[row][col])
@@ -56,5 +59,8 @@ print(dp)
 findMin()
 
 print(dp)
-# findWaht(4, 30)
+findWaht(4, 30)
+
+
+
 
