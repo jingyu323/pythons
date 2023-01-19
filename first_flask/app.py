d8a6d711e26c67c92c7d8a7be77b6afeee89f68e
jingyu323 <xzg323@163.com>
2023-01-20 05:52:38 +0800

    ss

--- first_flask/app.py
+++ first_flask/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 
+from test2 import test_share_var2
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    test_share_var2.share_var2()
+
+    print("main share valur is :"+test_share_var2.share_var2)
+
     return 'Hello World!'
 
 
---
+++ test/test_main.py
@@ -0,0 +1,8 @@
+
+def main():
+    print("this is main")
+
+
+
+if __name__ == "__main__":
+    main()
