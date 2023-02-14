f7e728ec480c3717d262afad66eecf9e74520f87
jingyu323 <xzg323@163.com>
2023-02-14 10:42:10 +0800

    mqtt

--- test/test_basic_grama/image_rec/test_easyocr.py
+++ test/test_basic_grama/image_rec/test_easyocr.py
@@ -2,10 +2,10 @@ import cv2
 import easyocr
 # reader = easyocr.Reader(['ch_sim','en'], gpu=True,download_enabled=True) # this needs to run only once to load the model into memory
 # reader = easyocr.Reader(['ch_sim', 'en'], gpu=False)
-reader = easyocr.Reader(['ch_sim', 'en'], gpu=False,download_enabled=False)
+reader = easyocr.Reader([ 'ch_tra','en'], gpu=False,download_enabled=False)
 # result = reader.readtext('test.png')
-result = reader.readtext('test2.jpg')
-# result = reader.readtext('chepai.png' ,detail = 0)
+# result = reader.readtext('test2.jpg')
+result = reader.readtext('chepai.png' ,min_size=1,detail = 0)
 for res in result:
      print(res)
 print("\n")
