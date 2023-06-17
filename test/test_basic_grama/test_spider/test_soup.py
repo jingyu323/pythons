26eb7e6be211c0d10aafd8e489cb8fb450b528a8
Rain <xzg323@163.com>
2023-06-17 15:05:16 +0800

    识别文字

--- test/test_basic_grama/test_spider/test_soup.py
+++ test/test_basic_grama/test_spider/test_soup.py
@@ -5,9 +5,11 @@ soup = BeautifulSoup('<p>Hello World</p>',"lxml")
 
 print(soup.p.string)
 
+# import tesserocr
 import pytesseract
 from PIL import Image
-path="E:\git_project\pythons\test\test_basic_grama\test_spider\test1.png"
+path="E:\\git_project\\pythons\\test\\test_basic_grama\\test_spider\\test1.png"
 image=Image.open(path)
 text=pytesseract.image_to_string(image,lang='chi_sim')
 print(text)
+
