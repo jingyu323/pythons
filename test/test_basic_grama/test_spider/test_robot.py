593a24851b17f449bceaaf4a3dd81bd5017b89e6
jingyu323 <xzg323@163.com>
2023-06-26 13:44:39 +0800

    request lib

--- test/test_basic_grama/test_spider/test_robot.py
+++ test/test_basic_grama/test_spider/test_robot.py
@@ -6,3 +6,26 @@ rp.read()
 print(rp)
 print(rp.can_fetch('*','http://www.jianshu.com/p/b67554025d7d'))
 print(rp.can_fetch('*',"http://www.jianshu.com/search?q=python&page=1&type=collections"))
+
+
+import requests
+
+data = {
+'name': 'germey',
+'age': 22}
+
+r = requests.get('https://www.baidu.com/',params=data)
+print(type(r))
+print(r.status_code)
+print (type(r.text))
+print(r.text)
+print(r.cookies)
+
+print("============r1==========")
+
+r1 = requests.post('http://httpbin.org/post')
+print(type(r1))
+print(r1.status_code)
+print (type(r1.text))
+print(r1.text)
+print(r1.cookies)
