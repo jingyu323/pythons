06b2693efce00c78a552e57d6d1e3016a12db135
Rain <xzg323@163.com>
2023-06-29 21:27:11 +0800

    python

--- test/test_basic_grama/basic_test/test_print.py
+++ test/test_basic_grama/basic_test/test_print.py
@@ -1,1 +1,16 @@
-print("sss")
+def Student():
+    name="susu"
+    age=21
+    print(f"{name}{age}了")
+    def School():
+        adress="河北"
+        banji=204
+        print(name+"在"+adress+"上学")
+    return School()
+Student()
+
+
+
+
+
+
