8a8b74132efe582e718446b3b4079d57234a0fec
jingyu323 <xzg323@163.com>
2023-07-04 18:03:49 +0800

    bar

--- test/test_basic_grama/charts/bar_chart_test.py
+++ test/test_basic_grama/charts/bar_chart_test.py
@@ -1,5 +1,5 @@
 from pyecharts.globals import ThemeType
-from pyecharts.charts import Bar3D
+from pyecharts.charts import Bar3D, Geo, Map
 from pyecharts import options as opts
 from pyecharts.charts import Bar
 from pyecharts.faker import Faker
@@ -118,6 +118,82 @@ def  scale_bar():
     bar.render_notebook()
     bar.render("scale_bar.html")
 
+def  geo_heatmap():
+    geo = Geo(init_opts=opts.InitOpts(theme='light',
+                                      width='1000px',
+                                      height='600px'))
+
+    geo.add_schema(maptype="china")
+
+    geo.add("",
+            [("广州", 150), ("成都", 70), ("南昌", 64), ("苏州", 100), ("郑州", 63)],
+            type_='heatmap')
+    # 热点图必须配置visualmap_opts
+    geo.set_global_opts(visualmap_opts=opts.VisualMapOpts())
+
+
+    geo.render_notebook()
+    geo.render()
+    geo.render("geo_heatmap.html")
+
+
+
+
+data = [('广东省', 129118.58),
+            ('山东省', 87435),
+            ('河南省', 61345),
+            ('四川省', 56749.80),
+            ('江苏省', 122875.60),
+            ('河北省', 42370.40),
+            ('湖南省', 48670.37),
+            ('安徽省', 45045),
+            ('湖北省', 53734.92),
+            ('浙江省', 77715),
+            ('广西壮族自治区', 26300.87),
+            ('云南省', 28954.20),
+            ('江西省', 32074.7),
+            ('辽宁省', 28975.1),
+            ('黑龙江省', 15901),
+            ('陕西省', 32772.68),
+            ('山西省', 25642.59),
+            ('福建省', 53109.85),
+            ('贵州省', 20164.58),
+            ('重庆市', 29129.03),
+            ('吉林省', 13070.24),
+            ('甘肃省', 11201.60),
+            ('内蒙古自治区', 23159),
+            ('台湾省', 51262.8),
+            ('上海市', 44652.8),
+            ('新疆维吾尔自治区', 17741.34),
+            ('北京市', 41610.9),
+            ('天津市', 16311.34),
+            ('海南省', 6818.22),
+            ('香港特别行政区', 23740),
+            ('宁夏回族自治区', 5069.57),
+            ('青海省', 3610.1),
+            ('西藏自治区', 2134.62),
+            ('澳门特别行政区', 1929.27)]
+
+def map_with_viusalmap():
+        map_chart = Map(init_opts=opts.InitOpts(theme='light',
+                                                width='1000px',
+                                                height='600px'))
+        map_chart.add('GDP（亿人民币）',
+                      data_pair=data,
+                      maptype='china',
+                      # 关闭symbol的显示
+                      is_map_symbol_show=False)
+
+        map_chart.set_global_opts(visualmap_opts=opts.VisualMapOpts(
+            max_=130000,  # visualmap默认映射数据范围是【0，100】，需调整
+            is_piecewise=True,
+            range_color=["#CCD3D9", "#E6B6C2", "#D4587A", "#DC364C"],
+        ))
+        map_chart.render_notebook()
+        map_chart.render("map_with_viusalmap.html")
+
+
+
 
 if __name__ == "__main__" :
-    scale_bar()
+    map_with_viusalmap()
