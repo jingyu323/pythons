fd7b486cab16aebede659bf4e0783ed0ad3f56c7
jingyu323 <xzg323@163.com>
2023-10-24 10:32:01 +0800

    read file

--- test/test_basic_grama/basic_test/test_open_file.py
+++ test/test_basic_grama/basic_test/test_open_file.py
@@ -2,19 +2,17 @@ with open('test.txt', 'r') as f:
     print(f.read())
 
 # 直接覆盖
-with open('test.txt', 'w') as f:
-    f.write('Hello, world!')
+with open('test.txt', 'a') as f:
+    f.write('Hello, world111!\n')
+    f.close()
 
 # 追加
 with open('test.txt', 'a') as f:
-    f.write('Hello, world!')
+    f.write('Hello, world222!\n')
+    f.close()
 
-    while True:
-        str = f.readline()
 
-        if not str:
-            break
-        print(str)
 
-        if str.__contains__("s"):
-            print("包含")
+with open('test.txt', 'r') as f:
+    for line in f:
+        print(line)
