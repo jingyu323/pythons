9a93705b351b35d4ea95e86d23b447519e1616e2
jingyu323 <xzg323@163.com>
2023-11-07 17:32:18 +0800

    mysql

--- test/test_basic_grama/test_request/gen_mysql_data.py
+++ test/test_basic_grama/test_request/gen_mysql_data.py
@@ -1,5 +1,24 @@
+import json
+
 import requests
 
 
-for i in range(1, 101):
-    response = requests.request('GET', 'http://localhost:8080/add')
+def inser_data():
+    for i in range(1, 101):
+        response = requests.request('GET', 'http://localhost:8080/add')
+        print(response.text)
+
+
+
+def get_all_data():
+    response = requests.request('GET', 'http://localhost:8080/all')
+    print(response.text )
+
+
+
+def get_data_by_parion(pation):
+    response = requests.request('GET', 'http://localhost:8080/allByPartion?pation='+pation)
+    print(response.text )
+
+if __name__ == '__main__':
+    get_data_by_parion("p2")
