966e2a53dbc4c3bc12fccfac7facb639afd6f254
jingyu323 <xzg323@163.com>
2024-05-22 18:24:21 +0800

    cal car

--- test/test_basic_grama/image_rec/opencv_test/basic/car_calc.py
+++ test/test_basic_grama/image_rec/opencv_test/basic/car_calc.py
@@ -17,15 +17,26 @@ def cal_car():
 
     cv2.destroyAllWindows()
 
+def center( x,y,w,h):
+    x1 = int(w/2)
+    y1= int(h/2)
+    cx = int(x) +x1
+    cy = int(y) +y1
+    return  cx,cy
+
+
 def cal_car_remove_background():
 
     cap  = cv2.VideoCapture("../video/gaosu.mp4")
 
     mog =cv2.createBackgroundSubtractorMOG2()
-    kenel = cv2.getStructuringElement(cv2.MORPH_RECT,(7,7))
+    kenel = cv2.getStructuringElement(cv2.MORPH_RECT,(9,9))
     min_w = 90
     min_h = 80
-    line_h = 400
+    line_h = 450
+    offset = 16
+    cars=[]
+    carcount=0
     while True:
         ret,fram = cap.read()
         if ret ==True:
@@ -45,18 +56,30 @@ def cal_car_remove_background():
 
             for cou in cous:
                 x,y,w,h =cv2.boundingRect(cou)
-                is_valid = (w>= min_w) &(h>= min_h)
+                is_valid = (w>= min_w) and (h>= min_h)
                 if not  is_valid:
                     continue
                 cv2.rectangle(fram,(int(x),int(y)),(int(x+w),int(y+h)),(0,255,0),2)
 
+                cp = center(x,y,w,h)
+                cv2.circle(fram,(cp),5,(0,0,255),-1)
+                cars.append(cp)
 
+                for (x,y) in cars:
+                    if y > (line_h-offset) and y < (line_h+offset):
+                        carcount +=1
+                        print(x,y)
+
+                        print(carcount)
+                    cars.remove((x, y))
+
+            cv2.putText(fram,"Vehicle Count:"+str(carcount),(500,60),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),3)
             cv2.imshow("fram",fram)
 
         key = cv2.waitKey(50)
         if key == 27:
             break
-
+    print(carcount)
     cap.release()
 
     cv2.destroyAllWindows()
