76a33024ac2407fd5e9a0a13621f1adbc059a14e
jingyu323 <xzg323@163.com>
2024-05-24 17:28:33 +0800

    color

--- test/test_basic_grama/image_rec/opencv_test/basic/corlor_test.py
+++ test/test_basic_grama/image_rec/opencv_test/basic/corlor_test.py
@@ -29,7 +29,63 @@ def face_masike():
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+def auto_face_masike():
+    cat = cv2.imread('../image/hezhao.jpeg')
+    print(cat.shape)
+    #  对于一个图片的二维数组，一个是高宽
+    # 所以对一个图片的截取 也是先从高度中取出一部分
 
+    #  人脸特征详细说明
+    face_detector= cv2.CascadeClassifier("../xml/haarcascade_frontalface_alt.xml")
+
+    faces = face_detector.detectMultiScale(cat,scaleFactor=1.14,minNeighbors=3)
+
+    print(faces)
+
+    star = cv2.imread('../image/star.jpeg')
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(cat,(x,y),(x+w,y+h),(0,255,0),2)
+
+        cat[y:y+h//4,x+(3*w//8):x+w//4+(3*w//8)] = cv2.resize(star,(w//4,h//4))
+
+
+    cv2.imshow("cat",cat)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def auto_face_removebg():
+    cat = cv2.imread('../image/hezhao.jpeg')
+    print(cat.shape)
+    #  对于一个图片的二维数组，一个是高宽
+    # 所以对一个图片的截取 也是先从高度中取出一部分
+
+    #  人脸特征详细说明
+    face_detector = cv2.CascadeClassifier("../xml/haarcascade_frontalface_alt.xml")
+
+    faces = face_detector.detectMultiScale(cat, scaleFactor=1.14, minNeighbors=3)
+
+    print(faces)
+
+    star = cv2.imread('../image/star.jpeg')
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(cat, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        re_star= cv2.resize(star, (w // 4, h // 4))
+        w1= w // 4
+        h1= h // 4
+        for i in range(w1):
+            for j in range(h1):
+                if not (re_star[i][j] > 180).all() :
+                    cat[ y +i,  j+x  + (3 * w // 8)]=re_star[i][j]
+
+        # cat[y:y + h // 4, x + (3 * w // 8):x + w // 4 + (3 * w // 8)] = cv2.resize(star, (w // 4, h // 4))
+
+    cv2.imshow("cat", cat)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    face_masike()
+    auto_face_removebg()
