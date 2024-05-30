3a35ead84e2792b06c05e931667beb5181f1b303
jingyu323 <xzg323@163.com>
2024-05-30 13:47:25 +0800

    ocr

--- test/test_basic_grama/image_rec/opencv_test/basic/car_number_detect.py
+++ test/test_basic_grama/image_rec/opencv_test/basic/car_number_detect.py
@@ -1,13 +1,74 @@
 import cv2
+import numpy as np
 
 
 def car_number_detect(img):
     car = cv2.imread(img)
     cv2.cvtColor(car, cv2.COLOR_BGR2GRAY)
     car_gray = cv2.cvtColor(car, cv2.COLOR_BGR2GRAY)
+    car_detect = cv2.CascadeClassifier("../xml/haarcascade_car_plate.xml")
+    plates = car_detect.detectMultiScale(car_gray)
+
+    for (x, y, w, h) in plates:
+        cv2.rectangle(car, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    cv2.imshow('car', car)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def lpr(filename): # Licence Plate recognize 车牌识别
+    # 1、图片加载和预处理
+    img = cv2.imread(filename)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY) # 灰度化处理
+    GaussianBlur_img = cv2.GaussianBlur(gray_img, (3, 3), sigmaX=0) # 高斯平滑，高斯模糊
+    canny = cv2.Canny(GaussianBlur_img,150,255) # 轮廓检测
+    ret, binary_img = cv2.threshold(canny, 127, 255, cv2.THRESH_OTSU) # 二值化操作
+
+    # 2、形态学运算
+    kernel = np.ones((5, 5), np.uint8)
+    # 先闭运算将车牌数字部分连接，再开运算较小的部分去掉
+    close_img = cv2.morphologyEx(binary_img, cv2.MORPH_CLOSE, kernel)
+    open_img = cv2.morphologyEx(close_img, cv2.MORPH_OPEN, kernel)
+    # 由于部分图像得到的轮廓边缘不整齐，因此再进行一次膨胀操作
+    dilation_img = cv2.dilate(open_img, np.ones(shape = [5,5],dtype=np.uint8), iterations=3)
+
+    # 3、获取轮廓
+    contours, hierarchy = cv2.findContours(dilation_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    rectangles = [] # 获取轮廓的矩形
+    for c in contours:
+        x = []
+        y = []
+        for point in c:
+            y.append(point[0][0])
+            x.append(point[0][1])
+        r = [min(y), min(x), max(y), max(x)]
+        rectangles.append(r)
+
+    # 4、根据HSV颜色空间查找汽车上车牌位置
+    dist_r = []
+    max_mean = 0
+    for r in rectangles:
+        block = img[r[1]:r[3], r[0]:r[2]] # block块
+        hsv = cv2.cvtColor(block, cv2.COLOR_BGR2HSV)
+        low = np.array([100, 43, 46])
+        up = np.array([124, 255, 255])
+        result = cv2.inRange(hsv, low, up)
+        # 用计算均值的方式找蓝色最多的区块
+        mean = np.mean(result)
+        if mean > max_mean:
+            max_mean = mean
+            dist_r = r
+    # 画出识别结果，由于之前多做了一次膨胀操作，导致矩形框稍大了一些，因此这里对于框架+3-3可以使框架更贴合车牌
+    cv2.rectangle(img, (dist_r[0]+3, dist_r[1]), (dist_r[2]-3, dist_r[3]), (0, 255, 0), 2)
+    cv2.imshow("lpr", img)
+    cv2.waitKey(0)
+# 主程序
 
 
 
 if __name__ == '__main__':
     image = "../../chepai.png"
-    car_number_detect(image);
+    # car_number_detect(image);
+
+    lpr(image)
--- test/test_basic_grama/image_rec/readme.md
+++ test/test_basic_grama/image_rec/readme.md
@@ -3,4 +3,24 @@
 EasyOCR 是一个用于从图像中提取文本的 python 模块, 它是一种通用的 OCR，既可以读取自然场景文本，也可以读取文档中的密集文本。
 利用cpu 识别文本
 
+安装  EasyOCR 后会有 cv2.imread 不能使用的问题
+解决方法：
+1.先卸载opencv
+
+pip uninstall opencv-python-headless
+pip uninstall opencv-python
+2.再下载opencv
+
+pip install opencv-python
+
+
+## 识别库
+### 光学字符识别 pytesseract  
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple  pytesseract
+
+
 # open-CV2
+
+# 学习地址
+## 各种识别方法
+https://geek-docs.com/opencv/python-opencv/t_how-to-check-if-an-image-contour-is-convex-or-not-in-opencv-python.html
