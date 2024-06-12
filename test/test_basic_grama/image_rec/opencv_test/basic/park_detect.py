c05fbf64ec7691c019834b002f9bd8b52c931608
jingyu323 <xzg323@163.com>
2024-06-12 22:16:19 +0800

    parking

--- test/test_basic_grama/image_rec/opencv_test/basic/park_detect.py
+++ test/test_basic_grama/image_rec/opencv_test/basic/park_detect.py
@@ -13,14 +13,14 @@ def park_detect():
     masked = parking.select_rgb_white_yellow(img)
 
     parking.cv_show("masked22", masked)
-    selectRegion= parking.select_region(masked)
+    selectRegion= parking.select_region(gray_img)
     edged = parking.detect_edges(selectRegion)
     parking.cv_show("selectRegion", selectRegion)
     parking.cv_show("edged", edged)
     parking_lines = parking.hough_lines(edged)
     print(parking_lines)
-    # new_img,spot_dict=parking.draw_parking(edged,parking_lines)
-    # parking.cv_show("new_img", new_img)
+    new_img,spot_dict=parking.draw_parking(selectRegion,parking_lines)
+    parking.cv_show("new_img", new_img)
 
 if __name__ == '__main__':
     park_detect()
