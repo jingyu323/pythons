79abe699304bb4f502323fbce75558787bbfd86b
jingyu323 <xzg323@163.com>
2024-06-16 09:52:35 +0800

    parking

--- test/test_basic_grama/image_rec/easyocr_demo.py
+++ test/test_basic_grama/image_rec/easyocr_demo.py
@@ -3,9 +3,9 @@ import easyocr
 # reader = easyocr.Reader(['ch_sim','en'], gpu=True,download_enabled=True) # this needs to run only once to load the model into memory
 reader = easyocr.Reader(['ch_sim', 'en'], gpu=False)
 # reader = easyocr.Reader([ 'ch_tra','en'], gpu=False,download_enabled=True)
-# result = reader.readtext('test.png')
+result = reader.readtext('test.png')
 # result = reader.readtext('test2.jpg')
-result = reader.readtext('chepai.png' ,min_size=1,detail = 0)
+# result = reader.readtext('chepai.png' ,min_size=1,detail = 0)
 for res in result:
      print(res)
 print("\n")
