2490293bb01f7a7067e9a6dfa8940ceca53f1163
jingyu323 <xzg323@163.com>
2024-06-23 09:58:23 +0800

    keras  stateful

--- test/test_basic_grama/image_rec/opencv_test/keras_study/sequential_demo.py
+++ test/test_basic_grama/image_rec/opencv_test/keras_study/sequential_demo.py
@@ -1,7 +1,7 @@
 import keras
 import numpy as np
-from keras import Sequential, Input
-from keras.src.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten
+from keras import Sequential, Input, Model
+from keras.src.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, Embedding, LSTM
 from keras.src.optimizers import SGD
 
 
@@ -125,9 +125,102 @@ def seq_multi_model_vgg():
     print(score)
 
 
-
 def seq_multi_model_lstm():
-    pass
+    data_dim = 16
+    timesteps = 8
+    num_classes = 10
+
+    # 期望输入数据尺寸: (batch_size, timesteps, data_dim)
+    model = Sequential()
+    model.add(LSTM(32, return_sequences=True,
+                   input_shape=(timesteps, data_dim)))  # 返回维度为 32 的向量序列
+    model.add(LSTM(32, return_sequences=True))  # 返回维度为 32 的向量序列
+    model.add(LSTM(32))  # 返回维度为 32 的单个向量
+    model.add(Dense(10, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    # 生成虚拟训练数据
+    x_train = np.random.random((1000, timesteps, data_dim))
+    y_train = np.random.random((1000, num_classes))
+
+    # 生成虚拟验证数据
+    x_val = np.random.random((100, timesteps, data_dim))
+    y_val = np.random.random((100, num_classes))
+
+    model.fit(x_train, y_train,
+              batch_size=64, epochs=5,
+              validation_data=(x_val, y_val))
+    score = model.evaluate(x_val, y_val, batch_size=16)
+    print(score)
+
+
+# 有问题没调试好
+def seq_multi_model_lstm2():
+    data_dim = 16
+    timesteps = 8
+    num_classes = 10
+    max_features = 1024
+    model = Sequential()
+    model.add(Embedding(max_features, output_dim=data_dim))
+    model.add(LSTM(data_dim))
+    model.add(Dropout(0.5))
+    model.add(Dense(10, activation='sigmoid'))
+    inputs = Input(name='inputs', shape=[max_features])
+    model = Model(inputs=inputs, outputs=model)  # 建立模型
+    model.summary()
+    model.compile(loss='binary_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+    # 生成虚拟训练数据
+    x_train = np.random.random((1000, timesteps, data_dim))
+    y_train = np.random.random((1000, num_classes))
+
+    # 生成虚拟验证数据
+    x_val = np.random.random((100, timesteps, data_dim))
+    y_val = np.random.random((100, num_classes))
+    model.fit(x_train, y_train, batch_size=16, epochs=10)
+    score = model.evaluate(x_val, y_val, batch_size=16)
+
+    print(score)
+
+def  seq_multi_model_stateful():
+    data_dim = 16
+    timesteps = 8
+    num_classes = 10
+    batch_size = 32
+
+    # 期望输入数据尺寸: (batch_size, timesteps, data_dim)
+    # 请注意，我们必须提供完整的 batch_input_shape，因为网络是有状态的。
+    # 第 k 批数据的第 i 个样本是第 k-1 批数据的第 i 个样本的后续。
+    model = Sequential()
+    model.add(LSTM(32, return_sequences=True, stateful=True,
+                   batch_input_shape=(batch_size, timesteps, data_dim)))
+    model.add(LSTM(32, return_sequences=True, stateful=True))
+    model.add(LSTM(32, stateful=True))
+    model.add(Dense(10, activation='softmax'))
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    # 生成虚拟训练数据
+    x_train = np.random.random((batch_size * 10, timesteps, data_dim))
+    y_train = np.random.random((batch_size * 10, num_classes))
+
+    # 生成虚拟验证数据
+    x_val = np.random.random((batch_size * 3, timesteps, data_dim))
+    y_val = np.random.random((batch_size * 3, num_classes))
+
+    model.fit(x_train, y_train,
+              batch_size=batch_size, epochs=5, shuffle=False,
+              validation_data=(x_val, y_val))
+
+    score = model.evaluate(x_val, y_val, batch_size=batch_size)
+
+    print(score)
 
 if __name__ == '__main__':
-    seq_multi_model_vgg()
+    seq_multi_model_lstm2()
