e8a27f3be2b383a79d267261a175353f870debdb
jingyu323 <xzg323@163.com>
2024-07-02 21:36:39 +0800

    flann

--- test/test_basic_grama/image_rec/opencv_test/basic/Stitcher.py
+++ test/test_basic_grama/image_rec/opencv_test/basic/Stitcher.py
@@ -8,6 +8,8 @@ class Stitcher:
         # 检测A、B图片的SIFT关键特征点，并计算特征描述子
         (kpsA, featuresA) = self.detectAndDescribe(imageA)
         (kpsB, featuresB) = self.detectAndDescribe(imageB)
+
+
         print("A shape:",imageA.shape)
         print("imageB shape:",imageB.shape)
 
@@ -63,13 +65,16 @@ class Stitcher:
 #
     def detectAndDescribe(self, image):
         # 将彩色图片转换成灰度图
-        # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # 计算特征点和描述子
 
         # 建立SIFT生成器
         # descriptor = cv2.SIFT_create()
         descriptor = cv2.SIFT_create()
         # 检测SIFT特征点，并计算描述子
-        (kps, features) = descriptor.detectAndCompute(image, None)
+        (kps, features) = descriptor.detectAndCompute(gray, None)
+
 
         # 将结果转换成NumPy数组
         kps = np.float32([kp.pt for kp in kps])
@@ -80,27 +85,47 @@ class Stitcher:
 
     def matchKeypoints(self, kpsA, kpsB, featuresA, featuresB, ratio, reprojThresh):
         # 建立暴力匹配器
-        matcher = cv2.BFMatcher()
+        # matcher = cv2.BFMatcher()
+        #
+        # # 使用KNN检测来自A、B图的SIFT特征匹配对，K=2
+        # rawMatches = matcher.knnMatch(featuresA, featuresB, 2)
 
-        # 使用KNN检测来自A、B图的SIFT特征匹配对，K=2
-        rawMatches = matcher.knnMatch(featuresA, featuresB, 2)
+        # 创建特征匹配器
+        index_params = dict(algorithm=1, trees=5)
+        search_params = dict(checks=50)
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        # 对描述子进行特征匹配
+        rawMatches = flann.knnMatch(featuresA, featuresB, k=2)  # 用的knnmatch匹配
+
+        print("rawMatches:", rawMatches)
 
         matches = []
-        for m in rawMatches:
+        for m,n in rawMatches:
             # 当最近距离跟次近距离的比值小于ratio值时，保留此匹配对
-            if  m[0].distance < m[1].distance * ratio:
-                print(m[0].distance, m[1].distance)
+            if  m.distance < n.distance * 0.75:
+                print(m.distance, n.distance)
+
                 # 存储两个点在featuresA, featuresB中的索引值
-                matches.append((m[0].trainIdx, m[0].queryIdx))
+                # matches.append((m[0].trainIdx, m[0].queryIdx))
+                matches.append(m)
 
         # 当筛选后的匹配对大于4时，计算视角变换矩阵
         if len(matches) >= 4:
             # 获取匹配对的点坐标
-            ptsA = np.float32([kpsA[i] for (_, i) in matches])
-            ptsB = np.float32([kpsB[i] for (i, _) in matches])
+            # ptsA = np.float32([kpsA[i] for (_, i) in matches])
+            # ptsB = np.float32([kpsB[i] for (i, _) in matches])
+            src_points = np.float32([kpsA[m.queryIdx] for m in matches]).reshape(-1, 1, 2)
+            des_points = np.float32([kpsB[m.trainIdx] for m in matches]).reshape(-1, 1, 2)
+
 
             # 计算视角变换矩阵
-            (H, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC, reprojThresh)
+            # (H, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC, reprojThresh)
+
+            # src_points = np.float32([kpsA[i] for (_, i) in matches])
+            # des_points = np.float32([kpsB[i] for  (i, _) in matches])
+
+            H, status = cv2.findHomography(src_points, des_points, cv2.RANSAC, 5)  # 参数5表示：允许有5个关键点的误差
 
             # 返回结果
             return (matches, H, status)
@@ -117,13 +142,13 @@ class Stitcher:
         vis[0:hB, wA:] = imageB
 
         # 联合遍历，画出匹配对
-        for ((trainIdx, queryIdx), s) in zip(matches, status):
-            # 当点对匹配成功时，画到可视化图上
-            if s == 1:
-                # 画出匹配对
-                ptA = (int(kpsA[queryIdx][0]), int(kpsA[queryIdx][1]))
-                ptB = (int(kpsB[trainIdx][0]) + wA, int(kpsB[trainIdx][1]))
-                cv2.line(vis, ptA, ptB, (0, 255, 0), 1)
+        # for ((trainIdx, queryIdx), s) in zip(matches, status):
+        #     # 当点对匹配成功时，画到可视化图上
+        #     if s == 1:
+        #         # 画出匹配对
+        #         ptA = (int(kpsA[queryIdx][0]), int(kpsA[queryIdx][1]))
+        #         ptB = (int(kpsB[trainIdx][0]) + wA, int(kpsB[trainIdx][1]))
+        #         cv2.line(vis, ptA, ptB, (0, 255, 0), 1)
 
         # 返回可视化结果
         return vis
