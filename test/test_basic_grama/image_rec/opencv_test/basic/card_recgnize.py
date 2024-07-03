df2310f92c01cd9fb476ad92c3aab0d4b6eb4e0d
jingyu323 <xzg323@163.com>
2024-07-03 18:28:57 +0800

    图修复

--- test/test_basic_grama/image_rec/opencv_test/basic/card_recgnize.py
+++ test/test_basic_grama/image_rec/opencv_test/basic/card_recgnize.py
@@ -585,12 +585,60 @@ def flnn_demo():
     img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
     cv_show('img33', img3)
 
+start_point=(0,0) #鼠标开始坐标
+lb_down = False #鼠标左键按下的标志，bool型
+def mouse_event(event, x, y, flags, param):
+    img =param[0]
+    mask =param[1]
+    global start_point, end_point, lb_down  # 如果全局变量是int或者str，那么如果想要在函数中对函数变量进行修改，则需要
+    # 先在函数内，声明其为global，再进行修改，如果是list或者dict则可以直接修改
+
+    if event == cv2.EVENT_LBUTTONDOWN:  # 左键按下，更新鼠标坐标，启动按下标志
+        start_point = (x, y)
+        lb_down = True
+
+    elif event == cv2.EVENT_MOUSEMOVE and lb_down:  # 鼠标移动，绘制线
+        cv2.line(img, start_point, (x, y), (255, 255, 255), thickness=5)
+        cv2.line(mask, start_point, (x, y), (255, 255, 255), thickness=5)
+        start_point = (x, y)  # 只要鼠标移动，就更新鼠标的坐标
+
+    elif event == cv2.EVENT_LBUTTONUP:  # 左键释放
+
+        cv2.line(img, start_point, (x, y), (255, 255, 255), thickness=5)  # 鼠标点击后直接释放鼠标的时候也会绘制一个点
+        cv2.line(mask, start_point, (x, y), (255, 255, 255), thickness=5)
+        lb_down = False
+
+def remove_word():
+    cv2.namedWindow('image')  # 新建窗口，用来进行鼠标操作
+    img = cv2.imread('../image/sb.png')
+    mask = np.zeros(img.shape, np.uint8)  # 创建一个黑色mask图像
+
+    pram=[img,mask]
+
+    cv2.setMouseCallback('image', mouse_event,pram)  # 设置鼠标回调
+
+    while True:
+        cv2.imshow('image', img)
+        cv2.imshow('mask', mask)
+
+        if cv2.waitKey(1) == ord('q'):  # waitKey参数不能写0，写0就需要键盘输入才会继续
+            break
+    img_inpaint_gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    print("============================")
+    result = cv2.inpaint(img, img_inpaint_gray, 3, cv2.INPAINT_TELEA)
+
+    cv2.imshow('img', np.hstack((img, result)))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 
 if __name__ == '__main__':
     # flnn_demo()
     # img_concat()
     # img_concat()
     # img_concat2()
-    img_concat3()
+    # img_concat3()
     # img_concat_lou()
     # swicher_concat_img()
+    remove_word()
