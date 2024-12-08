4879b3632f55855f69ac8c00e15b3788e023cbc6
jingyu323 <xzg323@163.com>
2024-12-09 07:46:49 +0800

    keras

--- pytorch_study/CNN/cnn_demo.py
+++ pytorch_study/CNN/cnn_demo.py
@@ -1,11 +1,11 @@
 from keras import Sequential
 from keras.src.datasets import mnist
-from keras.src.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
+from keras.src.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, SimpleRNN
 from keras.src.optimizers import SGD, Adam
 from keras.src.utils import to_categorical
 
 
-def demo1():
+def demo1_CNN():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     print("xshape", x_train.shape)
     print(x_train)
@@ -47,6 +47,47 @@ def demo1():
     print("train accuracy:", accuracy)
 
 
+def demo2_ltsm():
+    imput_size = 28
+    time_stemp =  28
+    # 隐藏层的个数
+    cell_size = 50
+
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    print("xshape", x_train.shape)
+    print(x_train)
+    # (60000,28,28 )
+
+    x_train = x_train / 255.0
+    x_test = x_test / 255.0
+    # x_train, x_test = x_train / 255.0, x_test / 255.0
+    # to onehot
+    y_train = to_categorical(y_train, 10)
+    y_test = to_categorical(y_test, 10)
+    # 创建784 个神经元 ,输出10 个神经元
+    model = Sequential()
+    model.add(SimpleRNN(units=cell_size,input_shape=(time_stemp, imput_size), return_sequences=True))
+    model.add(Dense(10, activation='softmax'))
+
+
+    #     定义优化器
+
+    adam = Adam(learning_rate=0.0001)
+
+    # 模型编译 使用交叉熵
+    model.compile(optimizer=adam, loss='categorical_crossentropy', metrics=['accuracy']);
+    # 模型训练
+    model.fit(x_train, y_train, epochs=30, batch_size=32, verbose=1)
+    #   模型评估
+    loss, accuracy = model.evaluate(x_test, y_test)
+    print("test loss:", loss)
+    print("test accuracy:", accuracy)
+
+    loss, accuracy = model.evaluate(x_train, y_train)
+    print("train loss:", loss)
+    print("train accuracy:", accuracy)
+
 
 if __name__ == '__main__':
-    demo1()
+    demo2_ltsm()
