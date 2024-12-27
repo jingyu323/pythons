0f39c66fcc662b82d35143b77095ccac8e61207c
jingyu323 <xzg323@163.com>
2024-12-27 18:06:50 +0800

    cat 识别

--- pytorch_study/Keras/cat_dog/cat_reginize.py
+++ pytorch_study/Keras/cat_dog/cat_reginize.py
@@ -22,63 +22,48 @@ validation_dogs_dir = os.path.join(validation_dir, 'dogs')
 
 model = Sequential([
     # 如果训练慢，可以把数据设置的更小一些
-     Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)),
+    Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 3)),
     MaxPooling2D(2, 2),
 
     Conv2D(64, (3, 3), activation='relu'),
     MaxPooling2D(2, 2),
 
-     Conv2D(128, (3, 3), activation='relu'),
+    Conv2D(128, (3, 3), activation='relu'),
     MaxPooling2D(2, 2),
 
     # 为全连接层准备
     Flatten(),
 
-     Dense(512, activation='relu'),
+    Dense(512, activation='relu'),
     # 二分类sigmoid就够了
     Dense(1, activation='sigmoid')
 ])
 model.summary()
 
-
 model.compile(loss='binary_crossentropy', optimizer=Adam(learning_rate=1e-4), metrics=['acc'])
 
-train_datagen = ImageDataGenerator(rescale=1./255)
-test_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = ImageDataGenerator(rescale=1. / 255)
+test_datagen = ImageDataGenerator(rescale=1. / 255)
 train_generator = train_datagen.flow_from_directory(
-        train_dir,  # 文件夹路径
-        target_size=(64, 64),  # 指定resize成的大小
-        batch_size=20,
-        # 如果one-hot就是categorical，二分类用binary就可以
-        class_mode='binary')
+    train_dir,  # 文件夹路径
+    target_size=(64, 64),  # 指定resize成的大小
+    batch_size=20,
+    # 如果one-hot就是categorical，二分类用binary就可以
+    class_mode='binary')
 
 validation_generator = test_datagen.flow_from_directory(
-        validation_dir,
-        target_size=(64, 64),
-        batch_size=20,
-        class_mode='binary')
-
-# 创建 tf.data.Dataset 对象
-dataset = tf.data.Dataset.from_generator(
-   train_generator,
-    output_signature=(
-        tf.TensorSpec(shape=(64,64), dtype=tf.float32),
-        tf.TensorSpec(shape=(), dtype=tf.int32)
-    )
-)
-
-# 批处理和数据打乱
-dataset = dataset.shuffle(buffer_size=1024).batch(20)
-
+    validation_dir,
+    target_size=(64, 64),
+    batch_size=20,
+    class_mode='binary')
 
 history = model.fit(
-    x=train_generator,
-      steps_per_epoch=100,  # 2000 images = batch_size * steps
-      epochs=20,
-      validation_data=validation_generator,
-      validation_steps=50,  # 1000 images = batch_size * steps
-      verbose=2)
-
+    train_generator,
+    steps_per_epoch=100,  # 2000 images = batch_size * steps
+    epochs=20,
+    validation_data=validation_generator,
+    validation_steps=50,  # 1000 images = batch_size * steps
+    verbose=2)
 
 acc = history.history['acc']
 val_acc = history.history['val_acc']
@@ -99,4 +84,3 @@ plt.title('Training and validation loss')
 plt.legend()
 
 plt.show()
-
