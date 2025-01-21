e9371767b8589d666435f33103ec6570916ae581
jingyu323 <xzg323@163.com>
2025-01-21 10:32:21 +0800

    one  hot

--- pytorch_study/tool/ImageDataGeneratorDemo.py
+++ pytorch_study/tool/ImageDataGeneratorDemo.py
@@ -1,3 +1,5 @@
+import cv2
+from PIL import Image
 from keras.src.legacy.preprocessing.image import ImageDataGenerator
 
 """
@@ -5,6 +7,7 @@ class_mode: "categorical", "binary", "sparse"或None之一. 默认为"categorica
 "binary"返回1D的二值标签."sparse"返回1D的整数标签,如果为None则不返回任何标签, 生成器将仅仅生成batch数据, 
 这种情况在使用model.predict_generator()和model.evaluate_generator()等函数时会用到.
 """
+
 
 def demo():
     datagen = ImageDataGenerator()
@@ -36,7 +39,30 @@ def demo():
     print(train_image_count)
 
 
+# 图片水平反转
+def image_change():
+    image_path = 'bird.png'
+    img = Image.open(image_path)
+    horizontal_flipped = img.transpose(method=Image.FLIP_LEFT_RIGHT)
+    horizontal_flipped.show()
+    # cv2.imshow("Image", horizontal_flipped)
+    # print("dddd")
+    # cv2.waitKey(0)
+
+
+def image_vertical_flip():
+    image_path = 'bird.png'
+    img = Image.open(image_path)
+
+    # 垂直翻转
+    vertical_flipped = img.transpose(method=Image.FLIP_TOP_BOTTOM)
+
+    # 保存翻转后的图像
+    vertical_flipped.save('vertical_flipped_image.jpg')
+
+    # 显示翻转后的图像 (可选)
+    vertical_flipped.show()
+
 
 if __name__ == '__main__':
-
-    demo()
+    image_vertical_flip()
