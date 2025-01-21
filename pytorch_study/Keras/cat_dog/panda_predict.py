e602f9caa6b2dae2168635ee332b227ac6e260e9
jingyu323 <xzg323@163.com>
2025-01-21 14:34:06 +0800

    panda  regz

--- pytorch_study/Keras/cat_dog/panda_predict.py
+++ pytorch_study/Keras/cat_dog/panda_predict.py
@@ -3,26 +3,26 @@ import pickle
 import cv2
 from keras.src.saving import load_model
 
-# --image images/dog.jpg --model output/simple\_nn.model --label-bin output/simple\_nn\_lb.pickle --width 32 --height 32 --flatten 1
-# --image images/dog.jpg --model output\_cnn/vggnet.model --label-bin output\_cnn/vggnet\_lb.pickle --width 64 --height 64
+# --image images/dog.jpg --model output/simple/_nn.model --label-bin output/simple/_nn/_lb.pickle --width 32 --height 32 --flatten 1
+# --image images/dog.jpg --model output/_cnn/vggnet.model --label-bin output/_cnn/vggnet/_lb.pickle --width 64 --height 64
 # 设置输入参数
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "images/dog.jpg", required=True,
-                help="path to input image we are going to classify")
-ap.add_argument("-m", "--model", required=True,
-                help="path to trained Keras model")
-ap.add_argument("-l", "--label-bin", required=True,
-                help="path to label binarizer")
-ap.add_argument("-w", "--width", type=int, default=28,
-                help="target spatial dimension width")
-ap.add_argument("-e", "--height", type=int, default=28,
-                help="target spatial dimension height")
-ap.add_argument("-f", "--flatten", type=int, default=-1,
-                help="whether or not we should flatten the image")
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "images/dog.jpg", required=True,
+#                 help="path to input image we are going to classify")
+# ap.add_argument("-m", "--model", required=True,
+#                 help="path to trained Keras model")
+# ap.add_argument("-l", "--label-bin", required=True,
+#                 help="path to label binarizer")
+# ap.add_argument("-w", "--width", type=int, default=28,
+#                 help="target spatial dimension width")
+# ap.add_argument("-e", "--height", type=int, default=28,
+#                 help="target spatial dimension height")
+# ap.add_argument("-f", "--flatten", type=int, default=-1,
+#                 help="whether or not we should flatten the image")
 
 flatten = 1
 # 加载测试数据并进行相同预处理操作
-image = cv2.imread("images/dog.jpg")
+image = cv2.imread("E:/data/kreas/Kaggle/cat-dog-small/train/pandas/panda_00004.jpg")
 output = image.copy()
 image = cv2.resize(image, (32, 64))
 
@@ -38,8 +38,8 @@ else:
                            image.shape[2]))
 # 读取模型和标签
 print("[INFO] loading network and label binarizer...")
-model = load_model("output/simple\_nn.mode")
-lb = pickle.loads(open("output/simple\_nn\_lb.pickle", "rb").read())
+model = load_model("cat_train.keras")
+lb = pickle.loads(open("label_bin", "rb").read())
 
 # 预测
 preds = model.predict(image)
--- pytorch_study/Keras/cat_dog/panda_regz.py
+++ pytorch_study/Keras/cat_dog/panda_regz.py
@@ -1,14 +1,14 @@
 import keras
-from keras_core import Sequential
-from keras_core.src.layers import Dense, Dropout
-from keras_core.src.optimizers import SGD
+from keras.src.layers import Dense, Dropout
+from keras.src.optimizers import SGD
+
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report#综合结果对比
+from sklearn.metrics import classification_report  # 综合结果对比
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
-from keras import initializers#初始化权重参数
-from keras import regularizers#正则化
+from keras import initializers, Sequential  # 初始化权重参数
+from keras import regularizers  # 正则化
 import matplotlib.pyplot as plt
 import numpy as np
 import random
@@ -20,7 +20,7 @@ from keras.src.utils import to_categorical, plot_model
 
 from Keras.cat_dog import utils_paths
 
-#--dataset --model --label-bin --plot
+# --dataset --model --label-bin --plot
 # 输入参数
 print("[INFO] 开始读取数据")
 data = []
@@ -32,25 +32,23 @@ random.seed(42)
 random.shuffle(imagePaths)
 # 遍历读取数据
 for imagePath in imagePaths:
-	# 读取图像数据，由于使用神经网络，需要输入数据给定成一维
-	image = cv2.imread(imagePath)
-	# 而最初获取的图像数据是三维的，则需要将三维数据进行拉长
-	image = cv2.resize(image, (32, 32)).flatten()
-	data.append(image)
+    # 读取图像数据，由于使用神经网络，需要输入数据给定成一维
+    image = cv2.imread(imagePath)
+    # 而最初获取的图像数据是三维的，则需要将三维数据进行拉长
+    image = cv2.resize(image, (32, 32)).flatten()
+    data.append(image)
 
-	# 读取标签，通过读取数据存储位置文件夹来判断图片标签
-	label = imagePath.split(os.path.sep)[-2]
-	labels.append(label)
+    # 读取标签，通过读取数据存储位置文件夹来判断图片标签
+    label = imagePath.split(os.path.sep)[-2]
+    labels.append(label)
 
 # scale图像数据，归一化
 data = np.array(data, dtype="float") / 255.0
 labels = np.array(labels)
 
-
-
 # 数据集切分
 (trainX, testX, trainY, testY) = train_test_split(data,
-	labels, test_size=0.25, random_state=42)
+                                                  labels, test_size=0.25, random_state=42)
 
 # 转换标签，one-hot格式
 lb = LabelBinarizer()
@@ -60,25 +58,28 @@ testY = lb.transform(testY)
 # trainY = le.fit_transform(trainY)
 # testY = le.transform(testY)
 # 将分类识别结果进行 数组类型的二进制 类别向量转换为二进制
-trainY=to_categorical(trainY)
-testY=to_categorical(testY)
+# trainY = to_categorical(trainY)
+# testY = to_categorical(testY)
 print(trainY)
 print(trainY.shape)
 
 # 网络模型结构：3072-512-256-3
 model = Sequential()
 
-
-print(len(2))
+print(len(lb.classes_))
 
 #
-model.add(Dense(512, input_shape=(3072,), activation="relu" ,kernel_initializer = initializers.TruncatedNormal(mean=0.0, stddev=0.05, seed=None),kernel_regularizer=regularizers.l2(0.01)))
+model.add(Dense(512, input_shape=(3072,), activation="relu",
+                kernel_initializer=initializers.TruncatedNormal(mean=0.0, stddev=0.05, seed=None),
+                kernel_regularizer=regularizers.l2(0.01)))
 model.add(Dropout(0.5))
-model.add(Dense(256, activation="relu",kernel_initializer = initializers.TruncatedNormal(mean=0.0, stddev=0.05, seed=None),kernel_regularizer=regularizers.l2(0.01)))
+model.add(
+    Dense(256, activation="relu", kernel_initializer=initializers.TruncatedNormal(mean=0.0, stddev=0.05, seed=None),
+          kernel_regularizer=regularizers.l2(0.01)))
 model.add(Dropout(0.5))
-model.add(Dense(len(2), activation="softmax",kernel_initializer = initializers.TruncatedNormal(mean=0.0, stddev=0.05, seed=None),kernel_regularizer=regularizers.l2(0.01)))
-
-
+model.add(Dense(len(lb.classes_), activation="softmax",
+                kernel_initializer=initializers.TruncatedNormal(mean=0.0, stddev=0.05, seed=None),
+                kernel_regularizer=regularizers.l2(0.01)))
 
 # 初始化参数
 INIT_LR = 0.001
@@ -88,20 +89,20 @@ EPOCHS = 30
 print("[INFO] 准备训练网络...")
 opt = SGD(learning_rate=INIT_LR)
 model.compile(loss="categorical_crossentropy", optimizer=opt,
-	metrics=["accuracy"])
+              metrics=["accuracy"])
 model.summary()
 # 训练网络模型
 
 print(trainX.shape, trainY.shape)
 print(testX.shape, testY.shape)
 H = model.fit(trainX, trainY, validation_data=(testX, testY),
-	epochs=EPOCHS, batch_size=32)
+              epochs=EPOCHS, batch_size=32)
 
 # 测试网络模型
 print("[INFO] 正在评估模型")
 predictions = model.predict(testX, batch_size=32)
 print(classification_report(testY.argmax(axis=1),
-	predictions.argmax(axis=1), target_names=lb.classes_))
+                            predictions.argmax(axis=1), target_names=lb.classes_))
 
 # 当训练完成时，绘制结果曲线
 N = np.arange(0, EPOCHS)
@@ -118,7 +119,7 @@ plt.savefig("cat_train ")
 # 保存模型到本地
 print("[INFO] 正在保存模型")
 model.save("cat_train.keras")
-f = open( "label_bin", "wb")
+
+f = open("label_bin", "wb")
 f.write(pickle.dumps(lb))
 f.close()
-
