e6fc203fc421089e65d2b150e2760554b08a0b40
jingyu323 <xzg323@163.com>
2025-01-24 15:47:32 +0800

    panda  regz

--- pytorch_study/tool/ImageDataGeneratorDemo.py
+++ pytorch_study/tool/ImageDataGeneratorDemo.py
@@ -1,6 +1,8 @@
 import cv2
+import numpy as np
 from PIL import Image
 from keras.src.legacy.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot as plt
 
 """
 class_mode: "categorical", "binary", "sparse"或None之一. 默认为"categorical. 该参数决定了返回的标签数组的形式,"categorical"会返回2D的one-hot编码标签,
@@ -64,5 +66,139 @@ def image_vertical_flip():
     vertical_flipped.show()
 
 
+def demo_img():
+    # 读取图像
+    img1 = cv2.imread("bare.png")
+    # 图像水平镜像
+    h, w = img1.shape[:2]
+    M = np.float32([[-1, 0, 500], [0, 1, 0]])
+    dst1 = cv2.warpAffine(img1, M, (w, h))
+
+    # 图像垂直镜像
+    h, w = img1.shape[:2]
+    M = np.float32([[1, 0, 0], [0, -1, 500]])
+    dst2 = cv2.warpAffine(img1, M, (w, h))
+
+    # 图像显示
+    fig, axes = plt.subplots(1, 3, figsize=(80, 10), dpi=100)
+    axes[0].imshow(img1[:, :, ::-1])
+    axes[0].set_title("original")
+    axes[1].imshow(dst1[:, :, ::-1])
+    axes[1].set_title("after horizontal-mirror")
+    axes[2].imshow(dst2[:, :, ::-1])
+    axes[2].set_title("after vertical-mirror")
+    plt.show()
+
+
+def demo1_image():
+    # 图像垂直镜像
+    img1 = cv2.imread("bare.png")
+    img2 = img1.copy()
+    img3 = img1.copy()
+
+    # 图像水平镜像
+    RM1 = np.float32([[1, 0, 0], [0, -1, 500], [0, 0, 1]])  # 计算出的旋转矩阵
+    RM2 = np.linalg.inv(RM1)  # 求解旋转矩阵的逆
+    for i in range(500):
+        for j in range(500):
+            D = np.dot(RM2, [[i], [j], [1]])  # 旋转后的图像坐标位置 相对应的 原图像坐标位置
+            if int(D[0]) >= 500 or int(D[1]) >= 500:  # 旋转后的图像坐标 相对应的 原图像坐标位置 越界
+                img2[i, j] = 0
+            elif int(D[0]) < 0 or int(D[1]) < 0:  # 旋转后的图像坐标 相对应的 原图像坐标位置 负值
+                img2[i, j] = 0
+            else:
+                img2[i, j] = img1[int(D[0]), int(D[1])]
+
+    # 图像垂直镜像
+    RM3 = np.float32([[-1, 0, 500], [0, 1, 0], [0, 0, 1]])  # 计算出的旋转矩阵
+    RM4 = np.linalg.inv(RM3)  # 求解旋转矩阵的逆
+    for i in range(500):
+        for j in range(500):
+            D = np.dot(RM4, [[i], [j], [1]])  # 旋转后的图像坐标位置 相对应的 原图像坐标位置
+            if int(D[0]) >= 500 or int(D[1]) >= 500:  # 旋转后的图像坐标 相对应的 原图像坐标位置 越界
+                img3[i, j] = 0
+            elif int(D[0]) < 0 or int(D[1]) < 0:  # 旋转后的图像坐标 相对应的 原图像坐标位置 负值
+                img3[i, j] = 0
+            else:
+                img3[i, j] = img1[int(D[0]), int(D[1])]
+
+    # 图像显示
+    fig, axes = plt.subplots(1, 3, figsize=(80, 10), dpi=100)
+    axes[0].imshow(img1[:, :, ::-1])
+    axes[0].set_title("original")
+    axes[1].imshow(img2[:, :, ::-1])
+    axes[1].set_title("after horizontal-mirror")
+    axes[2].imshow(img3[:, :, ::-1])
+    axes[2].set_title("after vertical-mirror")
+    plt.show()
+
+
+def demo2_image():
+    # 读取图像
+    img1 = cv2.imread("bare.png")
+    print(img1.shape)  # (1290, 1080, 3)
+    # 图像剪切
+    img2 = img1[420:800, 300:800]
+    print(img2.shape)  # (380, 500, 3)
+
+    # 图像显示
+    fig, axes = plt.subplots(1, 2, figsize=(10, 8), dpi=100)
+    axes[0].imshow(img1[:, :, ::-1])
+    axes[0].set_title("original")
+    axes[1].imshow(img2[:, :, ::-1])
+    axes[1].set_title("shear")
+
+    plt.show()
+
+
+"""
+flip(img,flipCode)
+
+img：就是对哪张图片进行翻转
+
+flipCode：flipCode==0 上下翻转  flipCode>0 左右翻转  flipCode<0上下＋左右 翻转
+"""
+
+
+def demo3_image():
+    import cv2
+    import numpy as np
+
+    ww = cv2.imread("bare.png")
+    shangxia = cv2.flip(ww, 0)
+    zuoyou = cv2.flip(ww, 1)
+    shangxiazuoyou = cv2.flip(ww, -1)
+
+    cv2.imshow('ww', ww)
+    cv2.imshow('shangxia', shangxia)
+    cv2.imshow('zuoyou', zuoyou)
+    cv2.imshow('shangxiazuoyou', shangxiazuoyou)
+
+    cv2.waitKey(0)
+
+
+"""
+图像旋转
+
+"""
+
+
+def demo4_image():
+    ww = cv2.imread("bare.png")
+
+    # 顺时针90
+    new = cv2.rotate(ww, cv2.ROTATE_90_CLOCKWISE)
+    # 旋转180
+    new1 = cv2.rotate(ww, cv2.ROTATE_180)
+    # 逆时针旋转90
+    new2 = cv2.rotate(ww, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+    cv2.imshow("ww", ww)
+    cv2.imshow('new', new)
+    cv2.imshow('new1', new1)
+    cv2.imshow('new2', new2)
+    cv2.waitKey(0)
+
+
 if __name__ == '__main__':
-    image_change()
+    demo4_image()
