4e88a09ca7c5099ce80ad4b196d333391cb5f83f
jingyu323 <xzg323@163.com>
2026-01-04 17:44:07 +0800

    mysql 读写

--- test_exam/py_mysql/mysql_cnn.py
+++ test_exam/py_mysql/mysql_cnn.py
@@ -28,6 +28,100 @@ def connect_mysql_use_result():
     #     print(row)
 
 
+def create_table():
+    db = MySQLdb.connect("localhost", "root", "root", "line4_240201")
+    # 使用cursor()方法获取操作游标
+    cursor = db.cursor()
+
+    # 如果数据表已经存在使用 execute() 方法删除表。
+    cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+
+    # 创建数据表SQL语句
+    sql = """CREATE TABLE EMPLOYEE \
+             ( \
+                 FIRST_NAME CHAR(20) NOT NULL, \
+                 LAST_NAME  CHAR(20), \
+                 AGE        INT, \
+                 SEX        CHAR(1), \
+                 INCOME     FLOAT \
+             )"""
+
+    cursor.execute(sql)
+
+    # 关闭数据库连接
+    db.close()
+
+def insert_data():
+    db = MySQLdb.connect("localhost", "root", "root", "line4_240201")
+    cursor = db.cursor()
+    # SQL 插入语句
+    sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+                                  LAST_NAME, AGE, SEX, INCOME)
+             VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+    try:
+        # 执行sql语句
+        cursor.execute(sql)
+        # 提交到数据库执行
+        db.commit()
+    except:
+        # Rollback in case there is any error
+        db.rollback()
+
+    # 关闭数据库连接
+    db.close()
+
+
+def select_data():
+    db = MySQLdb.connect("localhost", "root", "root", "line4_240201")
+    cursor = db.cursor()
+    sql = """SELECT * FROM EMPLOYEE"""
+    try:
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+    except:
+        print(  "Error: unable to fetch data")
+    # 关闭数据库连接
+    db.close()
+
+
+def update_data():
+    db = MySQLdb.connect("localhost", "root", "root", "line4_240201")
+    cursor = db.cursor()
+    # SQL 更新语句
+    sql = "UPDATE EMPLOYEE SET AGE = AGE + 1 WHERE SEX = '%c'" % ('M')
+    try:
+        # 执行SQL语句
+        cursor.execute(sql)
+        # 提交到数据库执行
+        db.commit()
+    except:
+        # 发生错误时回滚
+        db.rollback()
+
+    # 关闭数据库连接
+    db.close()
+
+def delete_data():
+    db = MySQLdb.connect("localhost", "root", "root", "line4_240201")
+    # 使用cursor()方法获取操作游标
+    cursor = db.cursor()
+
+    # SQL 删除语句
+    sql = "DELETE FROM EMPLOYEE WHERE AGE > %s" % (20)
+    try:
+        # 执行SQL语句
+        cursor.execute(sql)
+        # 提交修改
+        db.commit()
+    except:
+        # 发生错误时回滚
+        db.rollback()
+
+    # 关闭连接
+    db.close()
+
 
 
 
